02e9aae1b8231db5f23a0be67f59b523eded3877
Robo Atenaga <roboatenaga@gmail.com>
2017-06-07 21:13:28 +0700

    TableView and second ViewController added

--- EmoDic/ViewController.swift
+++ EmoDic/ViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tblEmojis: UITableView!
+    
+    var emojis = ["😘","😍","😂","👍🏾","😎","😒","🙄","🤓","😱","😩","😢"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let emojiCell = UITableViewCell()
+        emojiCell.textLabel?.text = emojis[indexPath.row]
+        return emojiCell
+    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
